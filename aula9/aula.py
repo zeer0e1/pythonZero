2b7f03631026a4735551a1b921bd5c488e825487
Lucas Gonçalves <lucasg@emphasisconf.com.br>
2023-03-24 11:06:56 -0300

    add novas aulas 10 - 16

---
+++ aula9/aula.py
@@ -0,0 +1,14 @@
+adicao = 10 + 10
+print('Adicao ', adicao)
+
+subtracao = 10 - 10
+print('subtracao ', subtracao)
+
+multiplicacao = 10 * 10
+print('multiplicacao ', multiplicacao)
+
+divisao = 10 / 2.2
+print('Adicao', adicao)
+
+divisao_inteira = 10 // 2.2
+print('Adicao', adicao)
