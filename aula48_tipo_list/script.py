f9735fed3969143815053facce550f0dea3ca69c
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-04-05 17:14:31 -0300

    novas aulae

---
+++ aula48_tipo_list/script.py
@@ -0,0 +1,13 @@
+"""
+Lista em python
+Tipo List - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizaveis -> indices e fatiamentos
+métodos uteis: append, insert, pop, del, clear, extend, +++
+"""
+
+string = 'ABCDE'
+lista = ['Texto', 123, True, 1.2, []]
+print(lista)
+lista[0] = 'Lucas'
+print(lista)
