307451583c78fca2b7506f46df253a6525cccb06
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-04-26 16:57:11 -0300

    'A'

---
+++ aula84_mapeamento_list_comprehsion/script.py
@@ -0,0 +1,26 @@
+# List comprehension em python
+# É uma forma simples e rápida para criar as listas
+# a partir de interáveis
+
+# Maneira de outras linguagens utilizar 
+lista1 = []
+
+for numero in range(10):
+  lista1.append(numero)
+
+# Maneira python de fazer isso 
+lista2 = [numero * 2 for numero in range(10)]
+
+print(lista2)
+
+# Mapeamento de dados em list comprehension
+
+produtos = [
+  {'nome': 'p1', 'preco':20,},
+  {'nome': 'p2', 'preco':10,}
+]
+
+print(produtos)
+novos_produtos = [ {**produto, 'preco': produto['preco'] * 1.05} if produto['nome'] == 'p1' else {**produto} for produto in produtos]
+
+print(novos_produtos)
