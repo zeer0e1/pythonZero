6a1d70499b37e9285588b105eb56a55920674d55
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-05-08 16:44:53 -0300

    Adicionando novas aulas

--- aula74_def_closure/script.py
+++ aula74_def_closure/script.py
@@ -4,11 +4,15 @@ Closure e funções que retornam outras funções
 
 
 def criar_saudacao(saudacao):
-  def saudar(nome):
-    return f'{saudacao}, {nome}!!'
-  return saudar
+    def saudar(nome):
+        return f'{saudacao},{nome}'
 
-s1 = criar_saudacao('Bom dia')
-s2 = criar_saudacao('Boa noite')
-print(s1('Lucas'))
-print(s2('Freitas'))
+    return saudar
+
+
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Boa noite')
+
+print(falar_bom_dia('Lucas'))
+print(falar_boa_noite('Lucas'))
+
