e8d0face18b78d24e2eebc5a57b15ffc3bf7ae8c
Lucas Ribeiro <lucasfreitasr@outlook.com>
2023-05-29 16:54:39 -0300

    Novas aulas

--- POO/aulaMixins/log.py
+++ POO/aulaMixins/log.py
@@ -2,14 +2,22 @@
 class Log:
   def log(self,msg):
     raise NotImplementedError('Implemente o método log')
+  
+  def log_error(self,msg):
+    return self._log(f'Error: {msg}')
+  
+  def log_sucess(self,msg):
+    return self._log(f'Error: {msg}')
 
 
 class LogFileMixin(Log):
-  def log(self,msg):
+  def _log(self,msg):
      print(msg)
 
-
+class LogPrintMixin(Log):
+  def _log(self,msg):
+     print(f'{msg}  ({self.__class__.__name__}) ')
 
 if __name__ == '__main__':
-  l = LogFileMixin()
-  l.log('Qualquer coisa') 
+  l = LogPrintMixin()
+  l.log_error('Qualquer coisa') 
